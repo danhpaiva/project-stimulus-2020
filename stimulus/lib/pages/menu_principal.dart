c2ab9fc8dd06f8eca222612998fcf44abb96eec4
Pujoni <gpujoni.dev@gmail.com>
2020-11-21 02:47:40 -0300

    Alterações de responsividade no menu principal

--- stimulus/lib/pages/menu_principal.dart
+++ stimulus/lib/pages/menu_principal.dart
@@ -13,65 +13,81 @@ class _MenuPageState extends State<MenuPage> {
   Widget build(BuildContext context) {
     return Material(
       color: Colors.white,
-      child: ListView(
-        children: <Widget>[
-          Container(
-            child: Column(
-              children: [
-                Container(
-                  width: 160,
-                  margin: EdgeInsets.only(bottom: 10),
-                  child: Text(
-                    'Stimulus',
-                    style: TextStyle(
-                        fontSize: 28,
-                        color: Color(0xFF011526),
-                        fontFamily: 'Century'),
-                    textAlign: TextAlign.center,
-                  ),
-                ),
-                ListViewAdvertinsing(),
-                GestureDetector(
-                  onTap: () {
-                    Navigator.push(
-                      context,
-                      MaterialPageRoute(
-                        builder: (context) => TrilhaCapacitacao(),
+      child: LayoutBuilder(
+        builder: (context, constraints) {
+          var screenWidth = constraints.maxWidth;
+          var screenHeight = constraints.maxHeight;
+
+          return ListView(
+            children: <Widget>[
+              Container(
+                child: Column(
+                  children: [
+                    Container(
+                      width: screenWidth * 0.45,
+                      height: screenHeight * 0.05,
+                      margin: EdgeInsets.only(
+                        top: screenHeight * 0.02,
                       ),
-                    );
-                  },
-                  child: MenuProcess(
-                      textTitleInfo:
-                          'Adquira conhecimento a partir dos nossos cursos',
-                      imageCard: 'trilha-01.png',
-                      titleCard: 'Capacitação Geral',
-                      descriptionCard:
-                          'Faça cursos para ampliar seus conhecimentos'),
+                      child: Text(
+                        'Stimulus',
+                        style: TextStyle(
+                            fontSize: 28,
+                            color: Color(0xFF011526),
+                            fontFamily: 'Century'),
+                        textAlign: TextAlign.center,
+                      ),
+                    ),
+                    Container(
+                      height: screenHeight * 0.23,
+                      margin: EdgeInsets.only(
+                          top: screenHeight * 0.01,
+                          bottom: screenHeight * 0.01),
+                      child: ListViewAdvertinsing(),
+                    ),
+                    Container(
+                      height: screenHeight - screenHeight * 0.38,
+                      child: Column(
+                        children: [
+                          GestureDetector(
+                            onTap: () {
+                              Navigator.push(
+                                  context,
+                                  MaterialPageRoute(
+                                      builder: (context) =>
+                                          TrilhaCapacitacao()));
+                            },
+                            child: MenuProcess(
+                                imageCard: 'trilha-01.png',
+                                titleCard: 'Capacitação Geral',
+                                descriptionCard:
+                                    'Faça cursos para ampliar seus conhecimentos'),
+                          ),
+                          GestureDetector(
+                            onTap: () {},
+                            child: MenuProcess(
+                                imageCard: 'monitoria-02.png',
+                                titleCard: 'Mentoria Específica',
+                                descriptionCard:
+                                    'Receba a mentoria de um empresário parceiro'),
+                          ),
+                          GestureDetector(
+                            onTap: () {},
+                            child: MenuProcess(
+                                imageCard: 'relatorio-03.png',
+                                titleCard: 'Sua experiência',
+                                descriptionCard:
+                                    'Avalie a eficácia do programa em seu negócio'),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ],
                 ),
-                GestureDetector(
-                  onTap: () {},
-                  child: MenuProcess(
-                      textTitleInfo:
-                          'Desenvolva novas percepções com seu mentor',
-                      imageCard: 'monitoria-02.png',
-                      titleCard: 'Mentoria Específica',
-                      descriptionCard:
-                          'Receba a mentoria de um empresário parceiro'),
-                ),
-                GestureDetector(
-                  onTap: () {},
-                  child: MenuProcess(
-                      textTitleInfo:
-                          'Relate os resultados do seu desenvolvimento',
-                      imageCard: 'relatorio-03.png',
-                      titleCard: 'Sua experiência',
-                      descriptionCard:
-                          'Avalie a eficácia do programa em seu negócio'),
-                ),
-              ],
-            ),
-          ),
-        ],
+              ),
+            ],
+          );
+        },
       ),
     );
   }
