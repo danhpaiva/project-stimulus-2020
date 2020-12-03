73100884343c2eee54c72cef8ce8ff57a73df0c6
Paiva <danpaiva@live.com>
2020-12-03 00:03:03 -0300

    Mentoria 01 | Okay

--- stimulus/lib/pages/mentoria/mentoria_especifica_01.dart
+++ stimulus/lib/pages/mentoria/mentoria_especifica_01.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:stimulus/pages/mentoria/mentoria_especifica_02.dart';
 
 class MentoriaEspecificaPage01 extends StatefulWidget {
   @override
@@ -16,9 +17,194 @@ class _MentoriaEspecificaPage01State extends State<MentoriaEspecificaPage01> {
       body: LayoutBuilder(
         builder: (context, constraints) {
           return Container(
+            margin: EdgeInsets.only(top: 20),
             child: Column(children: [
-              Text('Seu negócio melhorou após a utilização do Stimulus?'),
-              FloatingActionButton(onPressed: null)
+              Container(
+                //constraints.maxWidth * .8,
+                margin: EdgeInsets.only(top: 12, left: 20),
+                width: constraints.maxWidth * .9,
+                child: Column(
+                  children: [
+                    Container(
+                      width: constraints.maxWidth * .9,
+                      child: Text(
+                        'Endereço: *',
+                        style: TextStyle(
+                            color: Color(0xFF011526),
+                            fontSize: constraints.maxHeight * 0.03),
+                      ),
+                    ),
+                    Container(
+                      margin: EdgeInsets.only(top: 8),
+                      padding:
+                          EdgeInsets.only(top: 1, left: 6, right: 6, bottom: 2),
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.all(Radius.circular(8)),
+                        color: Colors.grey[200],
+                      ),
+                      height: constraints.maxHeight * 0.07,
+                      child: TextField(
+                        decoration: InputDecoration(labelText: 'Sua resposta'),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              Container(
+                //constraints.maxWidth * .8,
+                margin: EdgeInsets.only(top: 12, left: 20),
+                width: constraints.maxWidth * .9,
+                child: Column(
+                  children: [
+                    Container(
+                      width: constraints.maxWidth * .9,
+                      child: Text(
+                        'Telefone: *',
+                        style: TextStyle(
+                            color: Color(0xFF011526),
+                            fontSize: constraints.maxHeight * 0.03),
+                      ),
+                    ),
+                    Container(
+                      margin: EdgeInsets.only(top: 8),
+                      padding:
+                          EdgeInsets.only(top: 1, left: 6, right: 6, bottom: 2),
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.all(Radius.circular(8)),
+                        color: Colors.grey[200],
+                      ),
+                      height: constraints.maxHeight * 0.07,
+                      child: TextField(
+                        decoration: InputDecoration(labelText: 'Sua resposta'),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              Container(
+                //constraints.maxWidth * .8,
+                margin: EdgeInsets.only(top: 12, left: 20),
+                width: constraints.maxWidth * .9,
+                child: Column(
+                  children: [
+                    Container(
+                      width: constraints.maxWidth * .9,
+                      child: Text(
+                        'Razão Social: *',
+                        style: TextStyle(
+                            color: Color(0xFF011526),
+                            fontSize: constraints.maxHeight * 0.03),
+                      ),
+                    ),
+                    Container(
+                      margin: EdgeInsets.only(top: 8),
+                      padding:
+                          EdgeInsets.only(top: 1, left: 6, right: 6, bottom: 2),
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.all(Radius.circular(8)),
+                        color: Colors.grey[200],
+                      ),
+                      height: constraints.maxHeight * 0.07,
+                      child: TextField(
+                        decoration: InputDecoration(labelText: 'Sua resposta'),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              Container(
+                //constraints.maxWidth * .8,
+                margin: EdgeInsets.only(top: 12, left: 20),
+                width: constraints.maxWidth * .9,
+                child: Column(
+                  children: [
+                    Container(
+                      width: constraints.maxWidth * .9,
+                      child: Text(
+                        'CNPJ: *',
+                        style: TextStyle(
+                            color: Color(0xFF011526),
+                            fontSize: constraints.maxHeight * 0.03),
+                      ),
+                    ),
+                    Container(
+                      margin: EdgeInsets.only(top: 8),
+                      padding:
+                          EdgeInsets.only(top: 1, left: 6, right: 6, bottom: 2),
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.all(Radius.circular(8)),
+                        color: Colors.grey[200],
+                      ),
+                      height: constraints.maxHeight * 0.07,
+                      child: TextField(
+                        decoration: InputDecoration(labelText: 'Sua resposta'),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              Container(
+                //constraints.maxWidth * .8,
+                margin: EdgeInsets.only(top: 12, left: 20),
+                width: constraints.maxWidth * .9,
+                child: Column(
+                  children: [
+                    Container(
+                      width: constraints.maxWidth * .9,
+                      child: Text(
+                        'Idade da empresa: *:',
+                        style: TextStyle(
+                            color: Color(0xFF011526),
+                            fontSize: constraints.maxHeight * 0.03),
+                      ),
+                    ),
+                    Container(
+                      margin: EdgeInsets.only(top: 8),
+                      padding:
+                          EdgeInsets.only(top: 1, left: 6, right: 6, bottom: 2),
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.all(Radius.circular(8)),
+                        color: Colors.grey[200],
+                      ),
+                      height: constraints.maxHeight * 0.07,
+                      child: TextField(
+                        decoration: InputDecoration(labelText: 'Sua resposta'),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              Container(
+                margin: EdgeInsets.only(top: 20),
+                width: constraints.maxWidth * .5,
+                height: constraints.maxHeight * .06,
+                child: RaisedButton(
+                  shape: new RoundedRectangleBorder(
+                    side: BorderSide(
+                      color: Color(0xFF011526),
+                      width: 2,
+                      style: BorderStyle.solid,
+                    ),
+                    borderRadius: new BorderRadius.circular(9.0),
+                  ),
+                  onPressed: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                        builder: (context) => MentoriaEspecificaPage02(),
+                      ),
+                    );
+                  },
+                  child: Text(
+                    'Próxima',
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: constraints.maxWidth * 0.047,
+                    ),
+                  ),
+                  color: Color(0xFF011526),
+                ),
+              ),
             ]),
           );
         },
